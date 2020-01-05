5a1b4f3e9f8d891b5f30a97b9671c54f100b1edc
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-01-05 12:19:59 +0300

    30мин

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,85 @@
 const {Router} = require('express')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
-router.post('/register', (req, res) => {
+router.post(
+    '/register',
+    [
+        check('email', 'некорректный mail').isEmail(),
+        check('password', 'Минимальная длинна пароля 6 символов').isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Некоректные данные при регистрации'
+                })
+            }
+
+            const {email, password} = req.body
+
+            const candidate = await User.findOne({email})
+
+            if (candidate) {
+                return res.status(400).json({message: 'такой email уже существует'})
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const user = new User({email, password: hashedPassword})
+
+            await user.save()
+
+            res.status(201).json({message: 'пользователь создан'})
+
+        } catch (e) {
+            res.status(500).json({message: 'что то пошло не так, попробуйте снова'})
+        }
+    })
+
+router.post('/login', [
+    check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+    check('password', 'Введите пароль').exists()
+], async (req, res) => {
     try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Некоректные данные при входе'
+            })
+        }
+
         const {email, password} = req.body
+
+        const user = await User.findOne({email})
+
+        if (!user) {
+            return res.status(400).json({message: 'пользователь не найден'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({message: 'неверный пароль, поробуйте снова'})
+        }
+
+        const token = jwt.sign(
+            {userId: user.id},
+            config.get('jwtSecret'),
+            {expiresIn: '1h'}
+        )
+
+        await res.json({token, userId: user.id})
 
     } catch (e) {
         res.status(500).json({message: 'что то пошло не так, попробуйте снова'})
